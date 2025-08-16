6e938db4d43251cc986a5c796e933f1a4be103e7
TziviBlau <tzivib111@gmail.com>
2025-08-16 22:45:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -88,6 +88,10 @@ def delete_task(task_id):
     cnx.close()
     return redirect(url_for('index'))
 
+@app.route('/health')
+def health():
+    return "OK", 200
+
 if __name__ == '__main__':
     init_db()
     app.run(host='0.0.0.0', port=5000)
